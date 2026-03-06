610461ea8d63715291c57d97873805aa93c48b4e
natthapoom <natthapoomsaengkaew@gmail.com>
2026-03-06 10:41:33 +0700

    feat: Implement dynamic environment variable injection for VITE_GRAPHQL_URI and VITE_SERVER_URI in the frontend build.

---
+++ docker-entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+set -e
+
+VITE_GRAPHQL_URI="${VITE_GRAPHQL_URI:-http://100.48.64.156:8082/graphql}"
+VITE_SERVER_URI="${VITE_SERVER_URI:-http://100.48.64.156:8082}"
+
+find /usr/share/nginx/html/assets -name '*.js' -exec sed -i \
+  "s|__VITE_GRAPHQL_URI_PLACEHOLDER__|${VITE_GRAPHQL_URI}|g" {} +
+find /usr/share/nginx/html/assets -name '*.js' -exec sed -i \
+  "s|__VITE_SERVER_URI_PLACEHOLDER__|${VITE_SERVER_URI}|g" {} +
+
+exec nginx -g 'daemon off;'
